95ad4d45fc3d4153c943fd4087594b715c83292c
Venomhash123 <89146099+Venomhash123@users.noreply.github.com>
2022-08-03 08:33:23 +0530

    Update main2.py

--- main2.py
+++ main2.py
@@ -45,7 +45,7 @@ bot = Client(
 #)
 #auth_users = [ int(chat) for chat in os.environ.get("AUTH_USERS").split(",") if chat != '']
 #sudo_users = auth_users
-sudo_groups = [ int(chat) for chat in Sudo-groups.split(",")  if chat != '']
+sudo_groups = [ int(chat) for chat in Sudo_groups.split(",")  if chat != '']
 
 @bot.on_message(filters.command(["start"])&  (filters.chat(sudo_groups)))
 async def account_login(bot: Client, m: Message):
