3e7a45da0bcbeba63e036af9933e8f6aae3bfe66
Venomhash123 <89146099+Venomhash123@users.noreply.github.com>
2022-08-09 18:51:51 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = 18860540
 api_hash = "22dd2ad1706199438ab3474e85c9afab"
-bot_token = "5560046635:AAGghcWWbbZol0njystnRNdd4KV215jZDnM"
+bot_token = "5444138998:AAE23Em20ySpFIvbNUgDqHJcMm73vytp6qU"
 Sudo_groups = "5123176772,-1001600819423"
